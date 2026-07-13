dbfbf265d75a4a2377a71579ada9a45feb483166
ronitshrivas <ronit.shrivastav02@gmail.com>
2026-07-13 13:39:36 +0545

    Migration: ImagePathsCsv on Products

--- sabmero/Migrations/20260713075402_AddProductImagePathsCsv.Designer.cs
+++ sabmero/Migrations/20260713075402_AddProductImagePathsCsv.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using sabmero.Data;
@@ -11,9 +12,11 @@ using sabmero.Data;
 namespace sabmero.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260713075402_AddProductImagePathsCsv")]
+    partial class AddProductImagePathsCsv
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ sabmero/Migrations/20260713075402_AddProductImagePathsCsv.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sabmero.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImagePathsCsv : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePathsCsv",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImagePathsCsv",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImagePathsCsv",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImagePathsCsv",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImagePathsCsv",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$ISiZPRAsLzo7J10skoVCq.pW/FW4vaynEkkXD6gxWpLsSg2ph/Ssm");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePathsCsv",
+                table: "Products");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PasswordHash",
+                value: "$2a$11$Cy5vxd/1Z1aQpw58Gs0iMe0Kmpi8v/Q5S/IrhEUcUCTLEdfK6NQUu");
+        }
+    }
+}
